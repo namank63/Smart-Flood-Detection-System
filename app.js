978009348c39b0051d236a2d3292a209f1fda074
namank63 <48546593+namank63@users.noreply.github.com>
2021-03-15 13:28:29 +0530

    HomePage Flood Status
    -Added water sensor in seeding app
    -Added water sensor reading on homepage
    -UI Improved
    -Now we can see the flood status on homepage with live water sensor and ultrasonic sensor readings
    -

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     var query = req.query.search;
-    url = "https://api.thingspeak.com/channels/1305440/fields/1.json?api_key=9F03467TIMIAY1LI&results=1";
+    url = "https://api.thingspeak.com/channels/1305440/feeds.json?api_key=9F03467TIMIAY1LI&results=2";
+    // url = "https://api.thingspeak.com/channels/1305440/fields/1.json?api_key=9F03467TIMIAY1LI&results=2";
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body)
-            console.log(data);
-            // console.log(data.feeds[0].field1);
-            res.render("index.ejs", { data: data });
+            let ultrasonicSensorReading = Number(data.feeds[0].field1);
+            let walterSensorReading = Number(data.feeds[0].field2);
+            let colorLevel;
+            let floodStatus;
+            if (ultrasonicSensorReading >= 61) {
+                colorLevel = 'bg-success';
+                floodStatus = 'Normal';
+            } else if (ultrasonicSensorReading >= 31 && ultrasonicSensorReading <= 60) {
+                colorLevel = 'bg-warning';
+                floodStatus = 'Medium';
+            } else if (ultrasonicSensorReading <= 30 && walterSensorReading >= 70) {
+                colorLevel = 'bg-danger';
+                floodStatus = 'Danger';
+            }
+            res.render("index.ejs", { ultrasonicSensorReading: ultrasonicSensorReading, walterSensorReading: walterSensorReading, colorLevel: colorLevel, floodStatus: floodStatus });
         }
     });
 });
--- seed.js
+++ seed.js
@@ -2,16 +2,24 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-var query = 1;
+var ultrasonic = 100;
+var water = 100;
 var interval = 16 * 1000; //16 seconds
 for (var i = 0; i < 10; i++) {
     setTimeout(function (i) {
-        url = "https://api.thingspeak.com/update?api_key=PIHD73ES1100H0N3&field1=" + query;
+        url = "https://api.thingspeak.com/update?api_key=PIHD73ES1100H0N3&field1=" + ultrasonic + "&field2=" + water;
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var data = JSON.parse(body)
-                console.log("input count: " + data + " :: input value: " + query);
-                query = Math.floor(Math.random() * 100) + 1;
+                ultrasonic = Math.floor(Math.random() * 100) + 1;
+                if (ultrasonic <= 30) {
+                    water = Math.floor(Math.random() * (100 - 70) + 70);
+                    // water = Math.floor(Math.random() * 100) + 1;
+                } else {
+                    water = 0;
+                }
+                console.log("Ultrasonic Sensor input count: " + data + " :: Value: " + ultrasonic);
+                console.log("Water Sencer input count: " + data + " :: Value: " + water);
             }
         });
     }, interval * i, i);
