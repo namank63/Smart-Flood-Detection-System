f2f9a08b9c8cbd5067b9cfcbb2c4150ca5ff911b
namank63 <48546593+namank63@users.noreply.github.com>
2021-04-15 00:14:39 +0530

    Blog Modules and Admin panel added
    -Blog Modules added
    -Admin Panel added
    -Now Admin can CURD blogs
    -Users can see blog on home page

--- app.js
+++ app.js
@@ -35,15 +35,22 @@ mongoose.connect(process.env.DATABASE_API, { useUnifiedTopology: true, useNewUrl
 
 User = require("./models/user");
 Admin = require("./models/admin");
-Blog = require("./models/admin");
 
 
 
 /**********************************************
-ROUTES
+APP ROUTES
 **********************************************/
 //landing page
 app.get('/', (req, res) => {
+    var blogs;
+    Blog.find({}, function (err, allBlogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            blogs = allBlogs;
+        }
+    });
     var query = req.query.search;
     request(process.env.RETRIVE_THINGSPEAK_API, function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -72,7 +79,8 @@ app.get('/', (req, res) => {
             res.render("index.ejs", {
                 ultrasonicSensorReading: ultrasonicSensorReading,
                 walterSensorReading: walterSensorReading,
-                colorLevel: colorLevel, floodStatus: floodStatus
+                colorLevel: colorLevel, floodStatus: floodStatus,
+                blogs: blogs
             });
         }
     });
@@ -83,9 +91,93 @@ app.get('/login', (req, res) => {
     res.render("login.ejs");
 });
 
-//Admin Page
-app.get('/admin', (req, res) => {
-    res.render("admin.ejs");
+app.get("/admin", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("admin.ejs", { blogs: blogs });
+        }
+    });
+});
+
+
+/**********************************************
+BLOG ROUTES
+**********************************************/
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//New Route
+app.get("/new", function (req, res) {
+    res.render("new");
+});
+
+//Create Route
+app.post("/", function (req, res) {
+    //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            //then, redirect to the index
+            res.redirect("/admin");
+        }
+    });
+});
+
+//Show Route
+app.get("/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+//Edt Route
+app.get("/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.render("edit", { blog: foundBlog });
+        }
+    });
+});
+
+
+//Update Route
+app.put("/:id", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.redirect("/" + req.params.id);
+        }
+    });
+});
+
+//Delete Route
+app.delete("/:id", function (req, res) {
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.redirect("/");
+        }
+    });
+    //redirect somewhere
 });
 
 //Server
